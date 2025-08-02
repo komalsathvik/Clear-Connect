217ac5e4fdb385787b08510b1b25d98ff9676b52
rohith <rohithperugu3@gmail.com>
2025-08-02 18:03:59 +0530

    env url

--- frontend/src/pages/Videocall.jsx
+++ frontend/src/pages/Videocall.jsx
@@ -187,7 +187,7 @@ export default function Videocall() {
 
         socket.on("signal", onSignal);
 
-        socket.on("signal", onSignal);
+
 
         // 'user-left' event - cleanup peers on disconnect
         const onUserLeft = ({ userId }) => {
